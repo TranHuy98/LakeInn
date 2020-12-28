e47b1280afc47cdbfaf2db602d5c8657a2913887
Huy Tran <huy8663@gmail.com>
2020-12-28 17:21:12 +0700

    update 17h21 28/12/2020

--- js/detail-team.js
+++ js/detail-team.js
@@ -1,1 +1,10 @@
-detail-team.js
+$(document).ready(function(){
+	$('member-detail .member-img').mouseenter(function(){
+		$(this).find('.zoom-layer').css('animation-name','layerShow');
+		$(this).find('.zoom-btn').css('animation-name','btnShowUp');
+	})
+	$('member-detail .member-img').mouseleave(function(){
+		$(this).find('.zoom-layer').css('animation-name','layerHide');
+		$(this).find('.zoom-btn').css('animation-name','btnHideDown');
+	})
+});
