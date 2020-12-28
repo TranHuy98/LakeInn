cb4198906a64eaa25422364b248ce97e58a8958b
Huy Tran <huy8663@gmail.com>
2020-12-28 12:02:14 +0700

    update 12h02 28/12/2020

--- js/gallery-grid.js
+++ js/gallery-grid.js
@@ -1,8 +1,20 @@
+// layer
 $(document).ready(function(){
 	$('.gallery-grid .item').mouseenter(function(){
 		$(this).find('.info-layer').css('animation-name','infoShow');
 	});
 	$('.gallery-grid .item').mouseleave(function(){
 		$(this).find('.info-layer').css('animation-name','infoHide');
+	});
+});
+
+
+// button
+$(document).ready(function(){
+	$('.gallery-grid .item').mouseenter(function(){
+		$(this).find('.layer-item').css('animation-name','buttonShow');
+	});
+	$('.gallery-grid .item').mouseleave(function(){
+		$(this).find('.layer-item').css('animation-name','buttonHide');
 	});
 });
